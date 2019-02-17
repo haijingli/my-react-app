d775eeaf383e5265d95c6cf6e16d32a84f25038f
haijingli <lihaijing.1314@163.com>
2019-02-17 23:52:01 +0800

    添加es7的装饰器能力配置

--- config-overrides.js
+++ config-overrides.js
@@ -1,12 +1,21 @@
 const { injectBabelPlugin } = require('react-app-rewired');
 
 module.exports = function override(config, env) {
-    config = injectBabelPlugin([
-        'import', {
-            libraryName: 'antd',
-            libraryDirectory: 'es',
-            style: 'css'
-        }
-    ], config);
+    // antd按需加载
+    config = injectBabelPlugin(
+        [
+            'import', {
+                libraryName: 'antd',
+                libraryDirectory: 'es',
+                style: 'css'
+            }
+        ], config
+    );
+
+    //添加es7的装饰器能力
+    config = injectBabelPlugin(
+        ["@babel/plugin-proposal-decorators", { legacy: true }],
+        config
+    );
     return config;
 }
