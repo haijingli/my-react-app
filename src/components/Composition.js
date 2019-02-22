7be3e57c1967566ff3d11dcbac2e3f3b3fdbb5d1
haijingli <lihaijing.1314@163.com>
2019-02-22 16:01:02 +0800

    添加children的使用示例代码

--- src/components/Composition.js
+++ src/components/Composition.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 
 function Dialog(props) {
     return (
-        <div style={{ border: `2px solid ${props.color|| 'red'}`}}>
+        <div style={{ border: `2px solid ${props.color || 'red'}` }}>
             {/* 相当于vue中匿名插槽 */}
             {props.children}
             {/* 相当于vue中具名插槽 */}
@@ -11,9 +11,9 @@ function Dialog(props) {
     )
 }
 
-function WelcomDialog(props) {    
+function WelcomDialog(props) {
     //jsx
-    const  button = <button onClick={()=>alert("我是footer")}>确定</button>
+    const button = <button onClick={() => alert("我是footer")}>确定</button>
     return (
         <div>
             <Dialog color="green" footer={button}>
@@ -23,12 +23,77 @@ function WelcomDialog(props) {
         </div>
     )
 }
+
+const api = {
+    getUser: () => ({ name: 'Tom', age: 18 })
+}
+
+//props.children可以是任意合法的js表达式，包括函数。ps：jsx其实也是对js的扩展
+function Fetcher(props) {
+    let user = api[props.name]();
+    return props.children(user);
+}
+
+// 对props的children内容进行过滤
+function FilterP(props) {
+    return (
+        <div>
+            {/* React.Children提供若干操作嵌套内容的帮助方法 */}
+            {React.Children.map(props.children, child => {
+                console.log(child);//vdom                
+                if (child.type !== 'p') {
+                    return;
+                }
+                return child;
+            })}
+        </div>
+    )
+}
+
+function RadioGroup(props) {
+    return (
+        <div>
+            {
+                React.Children.map(props.children, child => {
+                    return React.cloneElement(child,{name:props.name})
+                })
+            }
+        </div>
+    )
+}
+
+function Radio({ children, ...rest }) {
+    return (
+        <label>
+            <input type="radio" {...rest} />{children}
+        </label>
+    )
+}
 // 组件复合，用复合实现组件的复用而非继承
 export default class Composition extends Component {
     render() {
         return (
             <div>
                 <WelcomDialog />
+                {/* Fetcher 相当于封装了后台查询请求，传入要调用的方法(getUser)，
+                嵌套的内容是个回调函数，该函数接收返回的查询结果并展示 */}
+                <Fetcher name="getUser">
+                    {({ name, age }) => <p>{name}-{age}</p>}
+                </Fetcher>
+
+                {/* 过滤children内容 */}
+                <FilterP>
+                    <h1>React</h1>
+                    <p>React很好用</p>
+                    <h1>Vue</h1>
+                    <p>Vue也不错</p>
+                </FilterP>
+
+                <RadioGroup name="mvvm">
+                    <Radio value="Vue">Vue</Radio>
+                    <Radio value="React">React</Radio>
+                    <Radio value="Angular">Angular</Radio>
+                </RadioGroup>
             </div>
         )
     }
